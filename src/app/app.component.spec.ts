ef41fd869ecf9694a277f50887547053daab127f
Nistha Raj <nistha.raj01@gmail.com>
2020-12-08 12:20:46 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'feedbackapp'`, () => {
+  it(`should have as title 'feedback'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('feedbackapp');
+    expect(app.title).toEqual('feedback');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('feedbackapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('feedback app is running!');
   });
 });
